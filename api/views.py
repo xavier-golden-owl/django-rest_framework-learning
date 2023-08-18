e7b2f8abaf7a5ca6a3402a2899fef88cfb680b1e
Cao Tan Duc <89432129+caotanduc@users.noreply.github.com>
2023-08-18 10:49:02 +0700

    generics class based view

--- api/views.py
+++ api/views.py
@@ -5,33 +5,12 @@ from rest_framework import mixins
 from rest_framework import generics
 
 
-class BookList(mixins.ListModelMixin,
-	mixins.CreateModelMixin,
-	generics.GenericAPIView):
-
+class BookList(generics.ListCreateAPIView):
 	queryset = Book.objects.all()
 	serializer_class = BookSerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
-
-	def post(self, request, *args, **kwargs):
-		return self.create(request, *args, **kwargs)
 	
 
-class BookDetail(mixins.RetrieveModelMixin,
-	mixins.UpdateModelMixin,
-	mixins.DestroyModelMixin,
-	generics.GenericAPIView):
-
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Book.objects.all()
 	serializer_class = BookSerializer
-
-	def get(self, request, *args, **kwargs):
-		return self.retrieve(request, *args, **kwargs)
-
-	def put(self, request, *args, **kwargs):
-		return self.update(request, *args, **kwargs)
-
-	def delete(self, request, *args, **kwargs):
-		return self.destroy(request, *args, **kwargs)
