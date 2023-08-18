a5c69428f49ee75c5ce97e302f9ba7ffc0f86245
Cao Tan Duc <89432129+caotanduc@users.noreply.github.com>
2023-08-18 09:29:09 +0700

    initial commit

---
+++ api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+	title = models.CharField(max_length=200)
+	author = models.CharField(max_length=200)
+	release = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		ordering = ['-release']
+
+	def __str__(self):
+		return title
